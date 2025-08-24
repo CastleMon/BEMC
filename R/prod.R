11e2fb1b9479e9e85db2479a308a465bf92945e4
CastleMon <blancamonroy.96@gmail.com>
2025-08-24 14:30:32 +0200

    Initial commit

---
+++ R/prod.R
@@ -0,0 +1,29 @@
+#' @title Product
+#'
+#' @description
+#' \code{product} calculate the product of two numbers
+#'
+#' @param x The first number
+#' @param y The second number
+#'
+#' @return Returns the product of \code{x} and \code{y}.
+#'
+#'
+#' @examples
+#' # Generate example data
+#' set.seed(123)
+#' x <- 5
+#' y <- 4
+#' # Compute MDC with U-centering
+#' product <- prod(x,y)
+#'
+#'
+#' @export
+#'
+#'
+
+prod <- function(x,y){
+  x * y
+}
+
+
